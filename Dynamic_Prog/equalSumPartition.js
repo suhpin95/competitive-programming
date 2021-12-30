2c433cc00e6414ac45f6523c2ffc85bce8da979a
Suhas <spindiproli4798@sdsu.edu>
2021-12-30 11:11:16 -0800

    Addition of DP top down approach

--- Dynamic_Prog/equalSumPartition.js
+++ Dynamic_Prog/equalSumPartition.js
@@ -3,15 +3,15 @@
  * find if the array can be partitioned into two subsets such that the sum of elements in both subsets is equal
  */
 
-const arr = [1,5,12,5];
-
+const arr = [1,5,11,5];
+var sum = 0;
+for(var i = 0; i < arr.length; i++){
+    sum += arr[i];
+}
 const equalPartition = () =>{
-    var isParition = false, sum = 0;
+    var isParition = false; 
     // we need to find the sum of all elements in the array 
     // then we need to get the 2 of the sum
-    for(var i = 0; i < arr.length; i++){
-        sum += arr[i];
-    }
     // we need to use this to check if it's even
     if( sum %2 === 0){
         isParition = subsetSum( (sum/2) , arr, arr.length - 1 );
@@ -20,15 +20,32 @@ const equalPartition = () =>{
         // in equal lengths
     return isParition;
 }
+console.log("Total",sum)
+
+const dp = new Array(arr.length+1)
+            .fill(0)
+            .map(()=>new Array((sum / 2)+ 1).fill(0));
+for(var i = 0 ; i < arr.length + 1; i++){
+    dp[i][0] = true;
+}
+for(var i = 0 ; i < (sum/2) + 1; i++){
+    dp[0][i] = false;
+}
+
 const subsetSum = (targetSum ,array, lengthOfArray) =>{
     if(lengthOfArray === 0)return false;
-    if( targetSum === 0)return true;
+    if( targetSum === 0){
+        return dp[lengthOfArray][targetSum] =  true;
+    }
+    if(dp[lengthOfArray - 1][targetSum] != 0){
+        return dp[lengthOfArray - 1][targetSum]
+    }
     if(array[lengthOfArray - 1] <= targetSum ){
-        return subsetSum(targetSum - array[lengthOfArray - 1], array, lengthOfArray - 1) 
+        return dp[lengthOfArray - 1][targetSum] = subsetSum(targetSum - array[lengthOfArray - 1], array, lengthOfArray - 1) 
                 || 
                 subsetSum(targetSum, array, lengthOfArray - 1)
     } else{
-        return subsetSum(targetSum, array, lengthOfArray - 1)
+        return dp[lengthOfArray - 1][targetSum]= subsetSum(targetSum, array, lengthOfArray - 1)
     }
 }
 console.log(equalPartition())
