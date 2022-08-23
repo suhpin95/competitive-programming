24b5ddbd8232eaac9d4d3062690e9d20aeec4618
Suhas Pindiproli <suhpin95@gmail.com>
2022-08-22 23:19:31 -0700

    Implemented Deletion in HEAP

--- Heap/MinHeap/minHeap.js
+++ Heap/MinHeap/minHeap.js
@@ -12,7 +12,7 @@
 class Heap {
     constructor(size) {
         this.noOfNodes = 0;
-        this.heapArr=new Array(size+1);
+        this.heapArr=new Array(size);
         this.heapArr[0] = 0;
     }
 
@@ -25,6 +25,11 @@ class Heap {
         }
 
         this.heapArr[this.noOfNodes] = val;
+        this.heapify()
+
+    }// insertion
+
+    heapify(){
         let index = this.noOfNodes;
         let parent = index / 2;
         while(this.heapArr[index] <= this.heapArr[parent]){
@@ -32,19 +37,45 @@ class Heap {
             index = parent;
             parent = index / 2;
         }
-
-    }// insertion
+    }
 
     deletion(){
+        if(this.noOfNodes === 1)return 'No Elements to delete';
+
+        // swap the last element as the top element
+        [this.heapArr[1], this.heapArr[this.noOfNodes]] = [this.heapArr[this.noOfNodes], this.heapArr[1]];
+         // removing the content
+        this.heapArr[this.noOfNodes] = undefined;
         this.noOfNodes--;
-        // check swap nodes, check if it meets the criteria of the min heap
+
+        let index = 1
+        do{
+            let left = index * 2;
+            let right = (index * 2) + 1;
+            // check the lesser value of left and right and then
+            // make that element as parent
+            if(this.heapArr[index] > this.heapArr[left] || this.heapArr[index] > this.heapArr[right]) {
+                if (this.heapArr[left] < this.heapArr[right]) { // right is greater
+                    [this.heapArr[index], this.heapArr[left]] = [this.heapArr[left], this.heapArr[index]];
+                    index = left;
+                } else { //left is greater
+                    [this.heapArr[index], this.heapArr[right]] = [this.heapArr[right], this.heapArr[index]];
+                    index = right;
+                }
+            } else{
+                break;
+            }
+        }while(index <= Math.floor(this.noOfNodes / 2) && index > 1)
+
     }//deletion
 
     display(){
         console.log(this.heapArr)
     }
 
-
+    size(){
+        return this.noOfNodes;
+    }
 }
 
 let minHeap = new Heap(10);
@@ -58,5 +89,12 @@ function _init(){
     minHeap.insertion(17)
     minHeap.insertion(10)
 }
+
+
 _init();
+
 minHeap.display();
+
+minHeap.deletion();
+minHeap.display();
+console.log("No of elements in the heap",minHeap.size())
