91d4304e03711e26c5a363ff53b7ee76666c0741
Suhas Pindiproli <spindiproli4798@sdsu.edu>
2023-03-08 17:56:22 -0800

    Included dijkstra's solution as well

--- Greedy_algo/dijkstras.js
+++ Greedy_algo/dijkstras.js
@@ -12,10 +12,10 @@
  * Output: 700
  */
 
-let n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+let n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
 
 var findCheapestPrice = function(n, flights, src, dst, k) {
-    return bellManFord(n, flights, src, dst, k);
+    return dijkstra(n, flights, src, dst, k);
 };
 
 const bellManFord = (n, flights, src, dst, k) => {
@@ -35,6 +35,42 @@ const bellManFord = (n, flights, src, dst, k) => {
         prices = [...tempPrice];
     }
     return prices[dst] == Infinity ? -1: prices[dst];
+}
+
+const dijkstra = (n, flights, src, dst, k) => {
+    // build adjList
+    let graph = {};
+    for(let i = 0; i < n; i++){
+        graph[i] = new Array();
+    }
+    for(let [s, dest, w] of flights){
+        graph[s].push([dest, w])// push the code dest and weight for the traversal
+    }
+    let minCost = new Array(n).fill(Infinity);
+    minCost[src] = 0;
+
+    // push srcnode, destNode, weight into heap
+    let queue = [[src, 0, 0]]; // push the node and weight alongwith it
+    // this will act as heap for us
+    while(queue.length > 0){
+
+        let [node, cost, stops] = queue.shift();
+        if(node === dst)return cost
+
+        if(stops > k)continue;
+        for(let [d, weight] of graph[node]){
+            let newCost = cost + weight;
+            if(newCost < minCost[d]){
+                minCost[d] = newCost;
+                queue.push([d, newCost, stops+1]);
+                _sort();// heapifying it essentially
+            }
+        }
+    }
+    function _sort(){
+        queue.sort((a,b) => a[1]-b[1]);
+    }
+    return -1;
 }
 
 console.log(findCheapestPrice(n, flights, src, dst, k));
